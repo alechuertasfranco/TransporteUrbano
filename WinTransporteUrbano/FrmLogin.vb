b50ff8e10c650ba36890ec5b1ab7c0c96a53ae45
Jhanpoul23 <60276270+Jhanpoul23@users.noreply.github.com>
2021-02-17 17:34:42 -0930

    login 2.0

--- WinTransporteUrbano/FrmLogin.vb
+++ WinTransporteUrbano/FrmLogin.vb
@@ -11,18 +11,33 @@ Public Class FrmLogin
         Dim correo As String
         Dim contraseña As String
         Dim datos() As String
-        Dim rol As String
-        Dim usuario As String
+        Dim tipo As String
+        Dim rolU As String
+        Dim mensaje As String
+        Dim idUsuario As Integer
+        Dim idControlador As Integer
         correo = txtUsuario.Text
         contraseña = txtPassword.Text
         datos = UsuarioLN.listarUsuarios(correo, contraseña)
-        usuario = datos(0)
-        rol = datos(1)
-        If usuario <> "" Then
-            If rol = "Controlador" Then
+        idControlador = datos(0)
+        idUsuario = datos(1)
+        tipo = datos(2)
+        rolU = datos(3)
+        If tipo <> "" Then
+            If tipo = "Controlador" Then
+
                 FrmIndexControlador.Show()
-            ElseIf rol = "Admin" Then
-                FrmIndexAdmin.Show()
+                MessageBox.Show(idControlador.ToString, "Bienvenido", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            ElseIf tipo = "Usuario" Then
+                If rolU = "1" Then
+
+                    MessageBox.Show(idUsuario.ToString, "Bienvenido", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    FrmIndexAdmin.Show()
+                Else
+                    mensaje = "Hola Secretaria" + idUsuario.ToString
+                End If
+
             End If
         Else
             MessageBox.Show("Error-El usuario no se encuentra registrado", "Ventana de Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
