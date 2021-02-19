aee15767659808d7d1a71f01bb65006c3f5a975e
Drako2711 <drako2711xd@gmail.com>
2021-02-19 18:31:30 -0500

    Sad :v

--- WinTransporteUrbano/FrmConductor.vb
+++ WinTransporteUrbano/FrmConductor.vb
@@ -137,11 +137,4 @@ Public Class FrmConductor
             MsgBox("Solo Puede digitar numeros")
         End If
     End Sub
-
-    Private Sub txt_nroLicencia_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txt_nroLicencia.KeyPress
-        e.Handled = Not IsNumeric(e.KeyChar) And Not Char.IsControl(e.KeyChar)
-        If Not IsNumeric(e.KeyChar) And Not Char.IsControl(e.KeyChar) Then
-            MsgBox("Solo Puede digitar numeros")
-        End If
-    End Sub
 End Class
