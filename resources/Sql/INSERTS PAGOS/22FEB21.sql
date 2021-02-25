f43f9de0bb9ac25628f1dce4ef955b466a43a48d
alechuertasfranco <max.alec.huertas@gmail.com>
2021-02-24 22:19:08 -0500

    Create 22FEB21.sql

---
+++ resources/Sql/INSERTS PAGOS/22FEB21.sql
@@ -0,0 +1,289 @@
+-- FECHA 22/02/2021
+USE BD_TransporteUrbano
+GO
+
+-- INSERT BUSES CONTROL
+    -- INSERT BUSES CONTROL HOJA 1
+        -- INSERT BUSES CONTROL BUS 1
+            -- INSERT BUSES_CONTROL CONTROL 1 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 1)
+            -- INSERT PAGO_CONTROL CONTROL 1 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:00:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 2 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 2)
+            -- INSERT PAGO_CONTROL CONTROL 2 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:04:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 3 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 3)
+            -- INSERT PAGO_CONTROL CONTROL 3 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:06:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 4 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 4)
+            -- INSERT PAGO_CONTROL CONTROL 4 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:18:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 5 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 5)
+            -- INSERT PAGO_CONTROL CONTROL 5 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:26:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 6 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 6)
+            -- INSERT PAGO_CONTROL CONTROL 6 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:34:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 7 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 7)
+            -- INSERT PAGO_CONTROL CONTROL 7 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:44:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 8 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 8)
+            -- INSERT PAGO_CONTROL CONTROL 8 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:49:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 9 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 9)
+            -- INSERT PAGO_CONTROL CONTROL 9 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:54:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 10 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 10)
+            -- INSERT PAGO_CONTROL CONTROL 10 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:00:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 11 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 11)
+            -- INSERT PAGO_CONTROL CONTROL 11 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:00:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 12 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 12)
+            -- INSERT PAGO_CONTROL CONTROL 12 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:15:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 13 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 13)
+            -- INSERT PAGO_CONTROL CONTROL 13 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:21:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 14 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 14)
+            -- INSERT PAGO_CONTROL CONTROL 14 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:26:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 15 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 15)
+            -- INSERT PAGO_CONTROL CONTROL 15 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:34:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 16 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 16)
+            -- INSERT PAGO_CONTROL CONTROL 16 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:44:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 17 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 17)
+            -- INSERT PAGO_CONTROL CONTROL 17 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:54:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 18 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 18)
+            -- INSERT PAGO_CONTROL CONTROL 18 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 08:03:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 19 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 19)
+            -- INSERT PAGO_CONTROL CONTROL 19 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 08:12:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 20 - BUS 1 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 20)
+            -- INSERT PAGO_CONTROL CONTROL 20 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 08:22:00 AM',5), 0.5)
+        
+        -- INSERT BUSES CONTROL BUS 3
+            -- INSERT BUSES_CONTROL CONTROL 1 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 1)
+            -- INSERT PAGO_CONTROL CONTROL 1 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:00:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 2 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 2)
+            -- INSERT PAGO_CONTROL CONTROL 2 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:04:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 3 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 3)
+            -- INSERT PAGO_CONTROL CONTROL 3 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:06:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 4 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 4)
+            -- INSERT PAGO_CONTROL CONTROL 4 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:18:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 5 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 5)
+            -- INSERT PAGO_CONTROL CONTROL 5 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:26:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 6 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 6)
+            -- INSERT PAGO_CONTROL CONTROL 6 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:34:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 7 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 7)
+            -- INSERT PAGO_CONTROL CONTROL 7 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:44:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 8 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 8)
+            -- INSERT PAGO_CONTROL CONTROL 8 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:49:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 9 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 9)
+            -- INSERT PAGO_CONTROL CONTROL 9 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 06:54:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 10 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 10)
+            -- INSERT PAGO_CONTROL CONTROL 10 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:00:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 11 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 11)
+            -- INSERT PAGO_CONTROL CONTROL 11 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:00:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 12 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 12)
+            -- INSERT PAGO_CONTROL CONTROL 12 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:15:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 13 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 13)
+            -- INSERT PAGO_CONTROL CONTROL 13 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:21:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 14 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 14)
+            -- INSERT PAGO_CONTROL CONTROL 14 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:26:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 15 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 15)
+            -- INSERT PAGO_CONTROL CONTROL 15 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:34:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 16 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 16)
+            -- INSERT PAGO_CONTROL CONTROL 16 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:44:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 17 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 17)
+            -- INSERT PAGO_CONTROL CONTROL 17 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 07:54:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 18 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 18)
+            -- INSERT PAGO_CONTROL CONTROL 18 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 08:03:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 19 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 19)
+            -- INSERT PAGO_CONTROL CONTROL 19 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 08:12:00 AM',5), 0.5)
+            
+            -- INSERT BUSES_CONTROL CONTROL 20 - BUS 3 - HOJA 1
+            INSERT INTO BUSES_CONTROL (HCONT_Codigo, BUS_IdBus, CONT_IdControl)
+            VALUES ('HC1122FEB211', 1, 20)
+            -- INSERT PAGO_CONTROL CONTROL 20 - BUS 1 - HOJA 1
+            INSERT INTO PAGO_CONTROL (HCONT_Codigo, BUS_IdBus, COND_IdConductor, PC_Fecha, PC_Monto)
+            VALUES ('HC1122FEB211', 1, 1, convert(datetime,'22-02-21 08:22:00 AM',5), 0.5)
+
+-- END INSERT BUSES CONTROL 
