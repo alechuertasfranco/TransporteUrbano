e7b2c5a0e3b45dbafe4d5b353349f24607c659b1
alechuertasfranco <max.alec.huertas@gmail.com>
2021-02-27 16:33:21 -0500

    Corrección del segundo reporte tactico

--- resources/Sql/BASE DIM/ReporteTactico_PenalizacionesControl.sql
+++ resources/Sql/BASE DIM/ReporteTactico_PenalizacionesControl.sql
@@ -1,57 +1,49 @@
---Determinar monto de penalizacion que se registraron en cada control seleccionando la cantidad de controles que se quiere vizualizar, ademas ingrese como parametro la semana
 USE BD_DimTransporteUrbano
 GO
 
-create PROCEDURE sp_Penalizaciones_Controles
-	@orden		char(4),
-	@cantidad	integer,
-	@semana     int
+--Determinar monto de penalizacion que se registraron en cada control seleccionando la cantidad de controles que se quiere vizualizar, ademas ingrese como parametro la semana
+CREATE PROCEDURE sp_Penalizaciones_Controles
+	@orden		CHAR(4),
+	@cantidad	INTEGER,
+	@semana     INT
 AS
 	BEGIN
 
-	select Top (@cantidad) C.Codigo, AVG(HT.Penalizacion/HT.TotalVueltas) as Promedio
-	from hechoTransporte HT
-	inner join dimBus B				on B.keyBus=HT.keyBus
-	inner join dimTiempo T			on T.keyTiempo=HT.keyTiempo
-	inner join dimControl C		on C.keyControl=HT.keyControl
-	where T.Semana=@semana
-	group by C.Codigo
-	order by case WHEN  @orden = 'asc' THEN AVG(HT.Penalizacion/HT.TotalVueltas)  end  asc,
-			CASE WHEN @orden = 'desc' THEN AVG(HT.Penalizacion/HT.TotalVueltas) end  desc
+		SELECT TOP	(@cantidad) C.Codigo,
+					AVG(HT.Penalizacion/HT.TotalVueltas) AS Promedio
+		FROM hechoTransporte HT
+			INNER JOIN dimBus B				on B.keyBus=HT.keyBus
+			INNER JOIN dimTiempo T			on T.keyTiempo=HT.keyTiempo
+			INNER JOIN dimControl C			on C.keyControl=HT.keyControl
+		WHERE T.Semana=@semana
+		GROUP BY C.Codigo
+		ORDER BY CASE WHEN @orden = 'asc'  THEN AVG(HT.Penalizacion/HT.TotalVueltas) END  ASC,
+				 CASE WHEN @orden = 'desc' THEN AVG(HT.Penalizacion/HT.TotalVueltas) END  DESC
 	END
 GO
 
-
-create PROCEDURE SP_orden
-as
-BEGIN
-create table #Ordenes(orden char(4))
-insert into #Ordenes values ('asc')
-insert into #Ordenes values ('desc')
-end
-
-select orden 
-	from #Ordenes
-go
-
-execute SP_orden
-
-
-create PROCEDURE SP_Cantidad
-as
-BEGIN
-declare @cont int 
-set @cont=0
-end
-select ROW_NUMBER()over(ORDER BY dimControl.keyControl asc) as numero
-	from dimControl
-go
-
-execute SP_Cantidad
-
-create view V_Semanas
+CREATE PROCEDURE SP_Cantidad
 AS
-	SELECT distinct T.semana					as Semana
-		FROM dimTiempo T
+	BEGIN
+		DECLARE @cont int 
+		SET @cont=0
+	END
+	SELECT ROW_NUMBER()OVER(ORDER BY dimControl.keyControl ASC) AS numero
+		FROM dimControl
 GO
 			
+CREATE VIEW V_Meses
+AS
+	SELECT DISTINCT Mes
+	FROM dimTiempo
+GO
+
+CREATE PROCEDURE SP_SemanasMes
+	@Mes	as VARCHAR(12)
+AS
+	BEGIN
+		SELECT DISTINCT Semana
+		FROM dimTiempo
+		WHERE Mes = @Mes
+	END
+GO
